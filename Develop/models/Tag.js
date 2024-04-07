298b51f44ee001d28f650ef012e8cdb3cf3f7328
Austin Luper <austin.luper@gmail.com>
2024-04-07 16:50:24 -0400

    Defined columns within the Tag.js file and added relevant comments to this added code

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,12 +1,26 @@
+// Import necessary modules from Sequelize
 const { Model, DataTypes } = require('sequelize');
 
+// Import the Sequelize instance created in the connection.js file
 const sequelize = require('../config/connection.js');
 
+// Initialize Tag model (table) by extending off Sequelize's Model class
 class Tag extends Model {}
 
+// Set up fields and rules for Tag model
 Tag.init(
   {
-    // define columns
+    // Define the 'id' column
+    id: {
+      type: DataTypes.INTEGER,   // Data type is INTEGER
+      primaryKey: true,          // Set as primary key
+      autoIncrement: true,       // Enable auto-increment
+    },
+    // Define the 'tag_name' column
+    tag_name: {
+      type: DataTypes.STRING,    // Data type is STRING
+      allowNull: false,          // Doesn't allow null values
+    }
   },
   {
     sequelize,
