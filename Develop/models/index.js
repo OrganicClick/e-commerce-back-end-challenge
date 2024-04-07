f0ec3be46a88b19f4384f402849bd1c36ded8eae
Austin Luper <austin.luper@gmail.com>
2024-04-07 17:10:36 -0400

    Established the Sequelize associations between tables within the index.js file and added relevant comments to this new section of code

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,17 +1,34 @@
-// import models
+// Import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Define associations between models
 
-// Categories have many Products
+// Define association: Product belongs to Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // Use category_id as foreign key in Product table
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Define association: Category has many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // Use category_id as foreign key in Product table
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Define association: Product belongs to many Tags through ProductTag
+Product.belongsToMany(Tag, {
+  through: ProductTag, // Use ProductTag as intermediary table
+  foreignKey: 'product_id', // Use product_id as foreign key in ProductTag table
+});
 
+// Define association: Tag belongs to many Products through ProductTag
+Tag.belongsToMany(Product, {
+  through: ProductTag, // Use ProductTag as intermediary table
+  foreignKey: 'tag_id', // Use tag_id as foreign key in ProductTag table
+});
+
+// Export all models
 module.exports = {
   Product,
   Category,
