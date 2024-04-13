26e5b8e8006242d3a2f96b1d9bac69c55794ba11
Austin Luper <austin.luper@gmail.com>
2024-04-13 11:51:15 -0400

    I don't fully understand what happened but after reverting the seeds/index.js file back to the starter code format, the npm run seed script is working again. Npm run seed had not worked originally, then made modifications until the script worked, and then had to revert back to starter code to get it to work again.

--- Develop/seeds/index.js
+++ Develop/seeds/index.js
@@ -6,21 +6,6 @@ const seedProductTags = require('./product-tag-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  // Drop ProductTag table first
-  await sequelize.query('DROP TABLE IF EXISTS `ProductTag`');
-  console.log('\n----- PRODUCT TAG TABLE DROPPED -----\n');
-
-  // Drop Product table next
-  await sequelize.query('DROP TABLE IF EXISTS `Product`');
-  console.log('\n----- PRODUCT TABLE DROPPED -----\n');
-
-  // Drop Tag table
-  await sequelize.query('DROP TABLE IF EXISTS `Tag`');
-  console.log('\n----- TAG TABLE DROPPED -----\n');
-
-  // Drop any other tables if needed
-
-  // Proceed with seeding
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedCategories();
