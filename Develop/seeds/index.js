e7dd4a8cc23e2bef0479d0a63e0374e97422f7b9
Austin Luper <austin.luper@gmail.com>
2024-04-13 16:37:58 -0400

    Added logic to seeds/index.js file to temporarily disable foreign key checks because the node seeds/index.js script is breaking again for some reason and this seems to fix it

--- Develop/seeds/index.js
+++ Develop/seeds/index.js
@@ -9,6 +9,28 @@ const sequelize = require('../config/connection');
 
 // Define function to seed all tables
 const seedAll = async () => {
+  // Disable foreign key checks and drop tables. Re-enables foreign key constraints after tables dropped.
+  // This logic is being included because node seeds/index.js script keeps breaking for some reason.
+  sequelize.query('SET FOREIGN_KEY_CHECKS = 0;')
+  .then(() => {
+    // Drop ProductTag table
+    return sequelize.query('DROP TABLE IF EXISTS `ProductTag`;');
+  })
+  .then(() => {
+    // Drop Tag table
+    return sequelize.query('DROP TABLE IF EXISTS `Tag`;');
+  })
+  .then(() => {
+    // Re-enable foreign key checks
+    return sequelize.query('SET FOREIGN_KEY_CHECKS = 1;');
+  })
+  .then(() => {
+    console.log('Tables dropped successfully.');
+  })
+  .catch(error => {
+    console.error('Error dropping tables:', error);
+  });
+
   // Synchronize database schema and drop existing tables
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
